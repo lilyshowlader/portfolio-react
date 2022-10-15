8f4bc15c4907c81bd726b5e39f12a3a8bd2d5b1b
lilyshowlader <lilyshowlader@gmail.com>
2022-10-15 16:50:32 -0400

    add favicon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,9 @@ function Home() {
     </div>  
 
     <div className="flexbox-item-2">
+  
     <p className='mainfont1'>my name is</p>
-    <p className='mainfont'>lily shanta howlader</p>
+    <p className='mainfont'><span className="s">l</span><span className="h">i</span><span className="a">l</span><span className="s">y</span> <span className="s">s</span><span className="h">h</span><span className="a">a</span><span className="s">n</span><span className="h">t</span><span className="a">a</span> <span className="s">h</span><span className="h">o</span><span className="a">w</span><span className="s">l</span><span className="h">a</span><span className="a">d</span><span className="s">e</span><span className="h">r</span></p>
     <p className='mainfont1'> and I am a software engineer,</p>
     <br></br>
     <p>women's health advocate,</p>
