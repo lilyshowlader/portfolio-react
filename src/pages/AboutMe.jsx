ef42db310aa080cf665d64a4e49ae0fdb96d17eb
lilyshowlader <lilyshowlader@gmail.com>
2022-10-23 15:24:28 -0400

    fix padding about me

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -10,7 +10,7 @@ function AboutMe() {
         <p className="contactfont"><span className="s">a</span><span className="h">b</span><span className="a">o</span><span className="s">u</span><span className="h">t </span><span className="a">m</span><span className="s">e</span></p>
         <p className='resumemobile'> I am a solutions-focused software engineer. My background in public health has taught me how to diagnose and cater to the needs of people and the requested end product. My eye for design and fashion helps me to deliver, clean, readable applications in a minimalist and stylish way. My skills include Javascript, HTML, CSS, React, Express JS, MongoDB and Mongoose.</p>
         <br></br>
-        <p className='resumemobile'> In my free time, I enjoy spending quality time with my friends and family, trying out different foods, dancing, studying the German language and playing with my three beautiful fur babies - mia, maeby and maya.</p>
+        <p className='resumemobile2'> In my free time, I enjoy spending quality time with my friends and family, trying out different foods, dancing, studying the German language and playing with my three beautiful fur babies - mia, maeby and maya.</p>
       </div>
     </div>
     </>
