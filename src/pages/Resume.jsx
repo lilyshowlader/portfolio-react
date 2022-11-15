3af5b54295df0b8496f64aceff1c84681794467a
lilyshowlader <lilyshowlader@gmail.com>
2022-11-14 22:13:07 -0500

    update resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,7 +3,7 @@ function Resume() {
     <>
     <div className="resume-container">
       <img src="https://i.imgur.com/XN2dzvw.png" className="resumephoto" alt=""/>
-      <p className='resumefont'>click <a className='resumefont' href='https://drive.google.com/file/d/1Jn16kIfKDMVv2o_aP6EZZjyQ57MVIjCq/view?usp=sharing'><span className="s">h</span><span className="h">e</span><span className="a">r</span><span className="s">e</span></a> to</p> <p className='resumefont'>download my resume. </p>
+      <p className='resumefont'>click <a className='resumefont' href='https://drive.google.com/file/d/1ozKVIGFloSqUFcYcZ2ZLTVctwkpy3c8T/view?usp=sharing'><span className="s">h</span><span className="h">e</span><span className="a">r</span><span className="s">e</span></a> to</p> <p className='resumefont'>download my resume. </p>
     </div>
     </>
     
