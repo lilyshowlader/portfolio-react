dfb8603a92d0db5adaa3dba5e5a4611fc3600098
lilyshowlader <lilyshowlader@gmail.com>
2023-01-04 10:55:59 -0500

    update resume with projects

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,7 +3,7 @@ function Resume() {
     <>
     <div className="resume-container">
       <img src="https://i.imgur.com/XN2dzvw.png" className="resumephoto" alt=""/>
-      <p className='resumefont'>click <a className='resumefont' href='https://drive.google.com/file/d/1iLQu7Qco_WluoFFRbUruWM4kiIkRDjqp/view?usp=sharing'><span className="s">h</span><span className="h">e</span><span className="a">r</span><span className="s">e</span></a> to</p> <p className='resumefont'>download my resume. </p>
+      <p className='resumefont'>click <a className='resumefont' href='https://docs.google.com/document/d/1eHItiEmDlAeTkudP_adNuzdvFxwIs-MXIaA2h9yz0IM/edit?usp=sharing'><span className="s">h</span><span className="h">e</span><span className="a">r</span><span className="s">e</span></a> to</p> <p className='resumefont'>download my resume. </p>
     </div>
     </>
     
