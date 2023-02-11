44b4d965a196813c883cf4dffc79aa1edd42c99c
lilyshowlader <lilyshowlader@gmail.com>
2023-02-11 08:57:37 -0500

    update resume with mifemiso

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,10 +3,10 @@ function Resume() {
     <>
     <div className="resume-container">
       <img src="https://i.imgur.com/XN2dzvw.png" className="resumephoto" alt=""/>
-      <p className='resumefont'>click <a className='resumefont' href='https://drive.google.com/file/d/1fhLjY1h3yBbRHjhMWuREOPogqr-rmOg_/view?usp=sharing'><span className="s">h</span><span className="h">e</span><span className="a">r</span><span className="s">e</span></a> to</p> <p className='resumefont'>download my resume. </p>
+      <p className='resumefont'>click <a className='resumefont' href='https://drive.google.com/file/d/1mcY2QyK6OMEZijgUIWVX-g_eLpnP4XYw/view?usp=sharing'><span className="s">h</span><span className="h">e</span><span className="a">r</span><span className="s">e</span></a> to</p> <p className='resumefont'>download my resume. </p>
     </div>
     </>
-    
+  
   )
 }
 export default Resume;
